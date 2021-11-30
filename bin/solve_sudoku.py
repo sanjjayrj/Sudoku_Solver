a70418d3a2ab996ed6a51a2caee5dff80ea39462
n-d-u <nandakishores73@gmail.com>
2021-11-30 18:41:15 +0530

    Update solve_sudoku.py

--- bin/solve_sudoku.py
+++ bin/solve_sudoku.py
@@ -41,7 +41,7 @@ def create_grid(puzzle_str):
     lines = puzzle_str.replace(' ','').replace('\n','')
     # Converting it to a list of digits
     digits = list(map(int, lines))
-    # Turning it to a 9x9 numpy array
+    # Converting it to a 9x9 numpy array
     grid = np.array(digits).reshape(9,9)
     return grid
 
